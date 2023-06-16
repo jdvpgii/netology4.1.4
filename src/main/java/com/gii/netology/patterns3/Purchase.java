1ba114dbea59c6e537718a30c28192edf0d36ee8
jdvpgii <gii.jdvp@gmail.com>
2023-06-16 12:40:54 +0300

    pattern solid

---
+++ src/main/java/com/gii/netology/patterns3/Purchase.java
@@ -0,0 +1,11 @@
+package com.gii.netology.patterns3;
+
+public class Purchase {
+    protected String title;
+    protected int count;
+
+    public Purchase(String title, int count) {
+        this.title = title;
+        this.count = count;
+    }
+}
